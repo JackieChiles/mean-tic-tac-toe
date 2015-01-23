ee69f0c1017a44b51d390d236b67955aa21792c1
JackieChiles <dtgaynor@gmail.com>
2015-01-22 21:25:50 -0500

    Started with a clean project.

--- app/app.js
+++ app/app.js
@@ -1,35 +1,11 @@
+var express = require('express');
+var app = express();
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
+    res.render('../server/views/index.ejs', { title: 'Tic-tac-toe' });
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3000, function() {
+    console.log('Started tic-tac-toe...');
 });
