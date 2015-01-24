c575974ef399c9e483f42859c60a0c182115bade
JackieChiles <dtgaynor@gmail.com>
2015-01-24 15:37:26 -0500

    Added comments.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
+//Express settings
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public/views');
+
+//Express routes
 app.get('/', function (req, res) {
     res.render('index', { title: 'Tic-tac-toe!!!' });
 });
 
+//Express startup
 app.listen(3000, function() {
     console.log('Started tic-tac-toe on port 3000...');
 });
