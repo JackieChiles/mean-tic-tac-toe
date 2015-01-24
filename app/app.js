a3ba65a9c179a309929e9b33333df85d05673011
JackieChiles <dtgaynor@gmail.com>
2015-01-24 15:34:19 -0500

    Fixed routing.

--- app/app.js
+++ app/app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public/views');
 app.get('/', function (req, res) {
-    res.render('../server/views/index.ejs', { title: 'Tic-tac-toe' });
+    res.render('index', { title: 'Tic-tac-toe!!!' });
 });
 
 app.listen(3000, function() {
-    console.log('Started tic-tac-toe...');
+    console.log('Started tic-tac-toe on port 3000...');
 });
