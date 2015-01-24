500424aef96487866363dd9b892802e454199af2
JackieChiles <dtgaynor@gmail.com>
2015-01-24 16:39:37 -0500

    Wired up Angular.

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 //Express settings
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/public/views');
+app.set('views', path.join(__dirname, 'public', 'views'));
 
 //Express routes
 app.get('/', function (req, res) {
     res.render('index', { title: 'Tic-tac-toe!!!' });
+});
+app.get('/angular.js',function(req,res) {
+    res.sendFile(path.join(__dirname, 'node_modules', 'angular', 'angular.js'));
 });
 
 //Express startup
