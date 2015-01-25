0455000514e5704d78507c149e29bb10db9665f9
JackieChiles <dtgaynor@gmail.com>
2015-01-25 16:13:05 -0500

    Added play logic and winner checking for rows.

--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -19,6 +19,60 @@
     });
 
     app.controller('BoardController', function ($scope) {
-        $scope.boardSize = 3;
+        $scope.size = 3;
+        $scope.positions = (function () {
+            var row = 0;
+            var col = 0;
+            var positions = [];
+            
+            for (row = 0; row < $scope.size; row++) {
+                positions.push([]);
+                
+                for(col = 0; col < $scope.size; col++) {
+                    positions[row].push({
+                        row: row,
+                        col: col,
+                        value: row * $scope.size + col
+                    });
+                }
+            }
+            
+            return positions;
+        })();
+        console.log($scope.positions);
+        $scope.currentPlayer = 'X';
+        $scope.winner = function (row, col) {
+            var player = $scope.positions[row][col].value;
+            
+            //Check row
+            var rWinner = $scope.positions[row].filter(function (cell) {
+                return cell.value === player;
+            }).length === $scope.size;
+            
+            //TODO: Check column
+            
+            //TODO: Check diagonals
+            
+            return rWinner ? player : null;
+        };
+        $scope.play = function (cell) {
+            var winner = null;
+            var row = cell.row;
+            var col = cell.col;
+            
+            //Make the play at the selected position
+            $scope.positions[row][col].value = $scope.currentPlayer;
+            
+            //Check for a win
+            winner = $scope.winner(row, col);
+            
+            if(winner) {
+                alert(winner + " wins!!!");
+                //TODO: reset board
+            }
+            
+            //Change players
+            $scope.currentPlayer = $scope.currentPlayer === 'X' ? 'O' : 'X';
+        };
     });
 }());
